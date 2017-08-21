7eede788be60217a1ca8e16e9058f914ef842430
sdombkow <sdombkow@gmail.com>
2017-08-21 10:09:45 -0400

    More progress on interactive map and got rid of gradients because of banding

--- www/javascript/mapzen-whosonfirstdemofour.js
+++ www/javascript/mapzen-whosonfirstdemofour.js
@@ -60,7 +60,10 @@ var create_venue_card = function(place) {
 }
 
 // NOOP (we are using onprogress instead)
-var onsuccess = function() { return; };
+var onsuccess = function() { 
+	console.log("Succesful query");
+	return; 
+};
 
 // Just log errors to the JS console
 var onerror = function(rsp) {
@@ -69,12 +72,15 @@ var onerror = function(rsp) {
 
 // Take all the API results and show them on the map
 var onprogress = function(rsp) {
-    //console.log(rsp);
+    console.log(rsp);
+	if (rsp.places.length == 0) {
+		console.log("Empty Response");
+	}
     for (var i = 0; i < rsp.places.length; i++) {
         var place = rsp.places[i];
         show_venue(place);
 		create_venue_card(place);
-		console.log()
+		console.log("Place Instance")
     }
 };
 
@@ -98,10 +104,13 @@ function runWhosOnFirstAPI() {
 	
 	//Tutorial Method Posted in Lower Box
 	document.getElementById("tutorial-api-method").innerHTML = method;
-	document.getElementById("whosonfirst-tutorial-form-longitude").value = chosenLocation[0];
-	document.getElementById("whosonfirst-tutorial-form-latitude").value = chosenLocation[1];
+	document.getElementById("whosonfirst-tutorial-form-longitude").value = chosenLocation[1];
+	document.getElementById("whosonfirst-tutorial-form-latitude").value = chosenLocation[0];
 	document.getElementById("whosonfirst-tutorial-form-radius").value = 21;
-	document.getElementById("whosonfirst-tutorial-top-side-button").style.backgroundColor = "rgba(33,33,33,.5)";
+	document.getElementById("whosonfirst-tutorial-form-max-longitude").value = chosenLocation[1];
+	document.getElementById("whosonfirst-tutorial-form-max-latitude").value = chosenLocation[0];
+	document.getElementById("whosonfirst-tutorial-form-min-longitude").value = chosenLocation[1];
+	document.getElementById("whosonfirst-tutorial-form-min-latitude").value = chosenLocation[0];
 };
 
 function retrieveInputCoordinates() {
@@ -137,6 +146,162 @@ function retrieveInputCoordinates() {
 	document.getElementById("whosonfirst-tutorial-form-longitude").value = chosenParent;
 	document.getElementById("whosonfirst-tutorial-form-latitude").value = chosenParent;
 	document.getElementById("whosonfirst-tutorial-form-radius").value = chosenParent;
+	
+	console.log("Updated Input Selections");
+	
+	/*Using Filderstatt where Stuttgart has an airport, id is 101761809*/
+}
+
+function clickPointButton() {
+	if (document.getElementById("whosonfirst-tutorial-top-side-plane-button").classList.contains("whosonfirst-tutorial-top-side-button-activate")) {
+		document.getElementById("whosonfirst-tutorial-top-side-plane-button").classList.toggle("whosonfirst-tutorial-top-side-button-activate");
+		document.getElementById("whosonfirst-tutorial-input-plane-container").classList.toggle("whosonfirst-tutorial-input-bottom-container-activated");
+		document.getElementById("whosonfirst-tutorial-top-side-point-button").classList.toggle("whosonfirst-tutorial-top-side-button-activate");
+		setTimeout(function() {document.getElementById("whosonfirst-tutorial-input-point-container").classList.toggle("whosonfirst-tutorial-input-bottom-container-activated")},1600);
+	} else {
+		document.getElementById("whosonfirst-tutorial-top-side-point-button").classList.toggle("whosonfirst-tutorial-top-side-button-activate");
+		document.getElementById("whosonfirst-tutorial-input-point-container").classList.toggle("whosonfirst-tutorial-input-bottom-container-activated");
+	}
+}
+
+function clickPlaneButton() {
+	if (document.getElementById("whosonfirst-tutorial-top-side-point-button").classList.contains("whosonfirst-tutorial-top-side-button-activate")) {
+		document.getElementById("whosonfirst-tutorial-top-side-point-button").classList.toggle("whosonfirst-tutorial-top-side-button-activate");
+		document.getElementById("whosonfirst-tutorial-input-point-container").classList.toggle("whosonfirst-tutorial-input-bottom-container-activated");
+		document.getElementById("whosonfirst-tutorial-top-side-plane-button").classList.toggle("whosonfirst-tutorial-top-side-button-activate");
+		setTimeout(function() {document.getElementById("whosonfirst-tutorial-input-plane-container").classList.toggle("whosonfirst-tutorial-input-bottom-container-activated")},1600);
+	} else {
+		document.getElementById("whosonfirst-tutorial-top-side-plane-button").classList.toggle("whosonfirst-tutorial-top-side-button-activate");
+		document.getElementById("whosonfirst-tutorial-input-plane-container").classList.toggle("whosonfirst-tutorial-input-bottom-container-activated");
+	}
+}
+
+function deactivateTutorialInfo() {
+	document.getElementById("whosonfirst-tutorial-info-1").classList.toggle("whosonfirst-tutorial-info-container-deactivated");
+	setTimeout(function() {document.getElementById("whosonfirst-tutorial-info-1").classList.toggle("whosonfirst-tutorial-info-container-deactivated-nodisplay")},1600);
+	document.getElementById("whosonfirst-tutorial-results-container").classList.toggle("whosonfirst-tutorial-results-container-activated");
+	document.getElementsByClassName("leaflet-right")[1].classList.toggle("leaflet-right-activated");
+}
+
+function customSearch() {
+	console.log(document.getElementById("search-text-go").value);
+	
+	document.getElementById("whosonfirst-tutorial-result-box-container").innerHTML = "";
+	
+	while (document.getElementById("whosonfirst-tutorial-result-box-container").firstChild) {
+		document.getElementById("whosonfirst-tutorial-result-box-container").removeChild();
+	};
+	
+	console.log("Cleared all children")
+	// Setup the API key
+    mapzen.whosonfirst.api.set_handler('authentication', function() {
+        return api_key;
+    });
+    // Get all the venues in the Flatiron District
+    // See: https://mapzen.com/documentation/wof/methods/#whosonfirst.places.getDescendants
+    var search_terms = document.getElementById("search-text-go").value;
+	console.log("Search Terms: " + search_terms);
+    var method = 'whosonfirst.places.search';
+    var data = {
+        q: search_terms,
+		locality_id: 101933229,
+        per_page: 50,
+        extras: 'geom:' // this gets us lat/lng coords
+    };
+	console.log(data);
+	
+    // Ok now we actually call the API
+    mapzen.whosonfirst.api.execute_method(method, data, onprogress, onerror);
+	
+	//Tutorial Method Posted in Lower Box
+	document.getElementById("whosonfirst-tutorial-form-longitude").value = search_terms;
+	document.getElementById("whosonfirst-tutorial-form-latitude").value = search_terms;
+	document.getElementById("whosonfirst-tutorial-form-radius").value = search_terms;
+	
+	console.log("Updated Input Selections");
+	
+	/*Using Filderstatt where Stuttgart has an airport, id is 101761809*/
+}
+
+function getByLatLonRadius() {
+	
+	document.getElementById("whosonfirst-tutorial-result-box-container").innerHTML = "";
+	
+	while (document.getElementById("whosonfirst-tutorial-result-box-container").firstChild) {
+		document.getElementById("whosonfirst-tutorial-result-box-container").removeChild();
+	};
+	
+	console.log("Cleared all children")
+	// Setup the API key
+    mapzen.whosonfirst.api.set_handler('authentication', function() {
+        return api_key;
+    });
+    // Get all the venues in the Flatiron District
+    // See: https://mapzen.com/documentation/wof/methods/#whosonfirst.places.getDescendants
+    var search_latitude = document.getElementById("whosonfirst-tutorial-form-latitude").value;
+	var search_longtitude = document.getElementById("whosonfirst-tutorial-form-longitude").value;
+	var search_radius = document.getElementById("whosonfirst-tutorial-form-radius").value;
+	if (search_radius == null || search_radius == "") {
+		search_radius = 1;
+	}
+	
+	console.log("Search Terms: " + search_radius + search_latitude + search_longtitude);
+	
+    var method = 'whosonfirst.places.getNearby';
+    var data = {
+        latitude: search_latitude,
+		longitude: search_longtitude,
+		radius: search_radius,
+        per_page: 50,
+        extras: 'geom:' // this gets us lat/lng coords
+    };
+	console.log(data);
+	
+    // Ok now we actually call the API
+    mapzen.whosonfirst.api.execute_method(method, data, onprogress, onerror);
+	
+	
+	console.log("Updated Input Selections");
+	
+	/*Using Filderstatt where Stuttgart has an airport, id is 101761809*/
+}
+
+function getByMaxAndMinLatLonRadius() {
+	
+	document.getElementById("whosonfirst-tutorial-result-box-container").innerHTML = "";
+	
+	while (document.getElementById("whosonfirst-tutorial-result-box-container").firstChild) {
+		document.getElementById("whosonfirst-tutorial-result-box-container").removeChild();
+	};
+	
+	console.log("Cleared all children")
+	// Setup the API key
+    mapzen.whosonfirst.api.set_handler('authentication', function() {
+        return api_key;
+    });
+    // Get all the venues in the Flatiron District
+    // See: https://mapzen.com/documentation/wof/methods/#whosonfirst.places.getDescendants
+    var search_max_latitude = document.getElementById("whosonfirst-tutorial-form-max-latitude").value;
+	var search_min_latitude = document.getElementById("whosonfirst-tutorial-form-min-latitude").value;
+	var search_max_longtitude = document.getElementById("whosonfirst-tutorial-form-max-longitude").value;
+	var search_min_longtitude = document.getElementById("whosonfirst-tutorial-form-min-longitude").value;
+
+	console.log("Search Terms: max" + search_max_latitude + "min" + search_min_latitude + "max" + search_max_longtitude + "min" + search_min_longtitude);
+	
+    var method = 'whosonfirst.places.getIntersects';
+    var data = {
+        max_latitude: search_max_latitude,
+		min_latitude: search_min_latitude,
+		max_longitude: search_max_longtitude,
+		min_longitude: search_min_longtitude,
+        per_page: 50,
+        extras: 'geom:' // this gets us lat/lng coords
+    };
+	console.log(data);
+	
+    // Ok now we actually call the API
+    mapzen.whosonfirst.api.execute_method(method, data, onprogress, onerror);
+	
 	
 	console.log("Updated Input Selections");
 	
