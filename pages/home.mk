abd2d4d2fbe69e1c4b2050e5cc94825db4132dfe
straup <straup@gmail.com>
2018-01-25 13:15:06 -0800

    first pass at feb2 build

--- pages/home.mk
+++ pages/home.mk
@@ -3,12 +3,25 @@ home: home-build-pages
 home-build-pages: \
 	home-page \
 	home-state \
-	home-interns \
-	home-bundles
+	home-bundles \
+	home-what \
+	home-download \
 
 home-page:
 	@make CONTENT=home.html OUT=index.html build-page-level0
 
+home-what:
+	@make CONTENT=what/index.html \
+	      OUT=what/index.html \
+	      SUBNAV_DIR=what \
+	      build-page-level1
+
+home-download:
+	@make CONTENT=download/index.html \
+	      OUT=download/index.html \
+	      SUBNAV_DIR=what \
+	      build-page-level1
+	
 home-state:
 	@make CONTENT=state.html \
 	      OUT=state/index.html \
