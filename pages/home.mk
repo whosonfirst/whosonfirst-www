1246117a6488a7932d12bf1b7a08a7f39b896d79
thisisaaronland <straup@gmail.com>
2018-01-27 07:36:12 -0800

    add a 404 page per issue #43; add subnav/common and remove unnecessary subnav files

--- pages/home.mk
+++ pages/home.mk
@@ -6,7 +6,8 @@ home-build-pages: \
 	home-what \
 	home-download \
 	home-code \
-	home-api
+	home-api \
+	home-404 
 
 home-page:
 	@make CONTENT=home.html OUT=index.html build-page-level0
@@ -14,31 +15,31 @@ home-page:
 home-api:
 	@make CONTENT=api/index.html \
 		OUT=api/index.html \
-		SUBNAV_DIR=api \
+		SUBNAV_DIR=common \
+		build-page-level1
+
+home-404:
+	@make CONTENT=404.html \
+		OUT=404.html \
+	        SUBNAV_DIR=common \
 		build-page-level1
 
 home-code:
 	@make CONTENT=code/index.html \
 	      OUT=code/index.html \
-	      SUBNAV_DIR=code \
+	      SUBNAV_DIR=common \
 	      build-page-level1
 
 home-what:
 	@make CONTENT=what/index.html \
 	      OUT=what/index.html \
-	      SUBNAV_DIR=what \
+	      SUBNAV_DIR=common \
 	      build-page-level1
 
 home-download:
 	@make CONTENT=download/index.html \
 	      OUT=download/index.html \
-	      SUBNAV_DIR=download \
-	      build-page-level1
-
-home-state:
-	@make CONTENT=state.html \
-	      OUT=state/index.html \
-	      SUBNAV_DIR=state \
+	      SUBNAV_DIR=common \
 	      build-page-level1
 
 home-interns:
