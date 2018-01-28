c6a8d2398d6920dcb988948c9921629c4ffe1926
straup <straup@gmail.com>
2018-01-28 06:41:09 -0800

    add a page about URIs

--- pages/docs.mk
+++ pages/docs.mk
@@ -42,8 +42,9 @@ docs-build-pages: \
 	docs-contributing \
 	docs-licenses \
 	docs-processes \
-	docs-keyterms
+	docs-keyterms \
 	docs-spr \
+	docs-uri
 
 docs-download-properties:
 	@for prop in $(PROPERTY_LIST) ; do \
@@ -221,6 +222,13 @@ docs-spr:
 	      OUT=docs/spr/index.html \
 	      PAGE_TITLE='Standard Places Response (SPR)' \
 	      SUBNAV_LINK=spr \
+	      docs-build-page-level2
+
+docs-uris:
+	@make CONTENT=docs/uris.html \
+	      OUT=docs/uris/index.html \
+	      PAGE_TITLE='URIs' \
+	      SUBNAV_LINK=uris \
 	      docs-build-page-level2
 
 docs-placetypes:
